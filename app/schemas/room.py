9dfab34ecf6a50d85964f0cdd9aa61d7e9664368
mailovezhangxiao <3447558389@qq.com>
2026-06-02 22:01:42 +0800

    feat(schemas): 定义 RoomInfoRequest 基础结构体

--- app/schemas/room.py
+++ app/schemas/room.py
@@ -1,3 +1,10 @@
 """
 房间模块数据传输对象(DTO)定义
 """
+
+from typing import Optional
+from pydantic import BaseModel
+
+class RoomInfoRequest(BaseModel):
+    player_id: Optional[int] = None
+    room_id: int
